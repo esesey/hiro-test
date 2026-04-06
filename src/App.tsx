5bc5f1f6aed8f34d713d15105e9b87840f640ec3
esesey <esesey@yandex.ru>
2026-04-06 03:23:59 +0300

    feat: fonts + libs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import SuperPrizeIcon from "./assets/super-prize.svg?react";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <p className="font-kelly-slab">Рыпапап</p>
+      <p className="font-alumni-sans">Рыпапап</p>
+      <SuperPrizeIcon />
+    </>
+  );
 }
 
 export default App;
